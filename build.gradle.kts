4e1693ec17d881c3b9b1d5666cb16498018ffcde
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-11-03 13:22:17 -0700

    Init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val asm = "org.ow2.asm:asm:9.4"
+    api(asm)
+    testImplementation(asm)
+
+    val checker = "org.checkerframework:checker-qual:3.25.0"
+    compileOnlyApi(checker)
+    testCompileOnly(checker)
+}
+
+publishing.publications.create<MavenPublication>("maven") {
+    from(components["java"])
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = Charsets.UTF_8.name()
+    options.release.set(17)
+}
+tasks.withType<Javadoc> {
+    options.encoding = Charsets.UTF_8.name()
+}
+tasks.withType<ProcessResources> {
+    filteringCharset = Charsets.UTF_8.name()
+}
