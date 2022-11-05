4a9390e79063326399ed579453a7464fe84cc6bd
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-11-04 21:19:58 -0700

    Move EnumRuleReflectionProxy to upper level

--- runtime/src/main/java/io/papermc/reflectionrewriter/runtime/DefaultRulesReflectionProxy.java
+++ runtime/src/main/java/io/papermc/reflectionrewriter/runtime/DefaultRulesReflectionProxy.java
@@ -69,18 +69,4 @@ public interface DefaultRulesReflectionProxy {
     // Begin MethodType
     MethodType fromMethodDescriptorString(String descriptor, ClassLoader loader) throws IllegalArgumentException, TypeNotPresentException;
     // End MethodType
-
-    /**
-     * Has required methods for the non-default enum rule. Implement in addition to
-     * {@link DefaultRulesReflectionProxy} if using the enum rule.
-     */
-    interface EnumRuleReflectionProxy {
-        <E extends Enum<E>> E enumConstant(MethodHandles.Lookup lookup, String name, Class<E> type);
-
-        <E extends java.lang.Enum<E>> E valueOf(Class<E> enumClass, String name);
-
-        default <E extends java.lang.Enum<E>> E valueOf(final String name, final Class<E> enumClass) {
-            return this.valueOf(enumClass, name);
-        }
-    }
 }
