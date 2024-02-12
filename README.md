b6222b2a79ecec0b8ce6a188f698c581ad422a45
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-02-12 11:43:47 -0700

    restructure

---
+++ README.md
@@ -0,0 +1,14 @@
+# paper-asm
+
+Houses ASM utilities for Paper projects
+
+## paper-asm (root project)
+
+Shared ASM utilities
+
+## reflection-rewriter
+
+reflection-rewriter is composed of three components:
+- rewriter: ASM visitors for redirecting reflection calls
+- runtime: implements targets for redirected reflection calls
+- proxy-generator: generates static proxies for runtime
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,13 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-rootProject.name = "reflection-rewriter"
+rootProject.name = "paper-asm"
 
-include("proxy-generator")
-project(":proxy-generator").name = rootProject.name + "-proxy-generator"
+include("reflection-rewriter/rewriter")
+project(":reflection-rewriter/rewriter").name = "reflection-rewriter"
 
-include("runtime")
-project(":runtime").name = rootProject.name + "-runtime"
+include("reflection-rewriter/proxy-generator")
+project(":reflection-rewriter/proxy-generator").name = "reflection-rewriter-proxy-generator"
+
+include("reflection-rewriter/runtime")
+project(":reflection-rewriter/runtime").name = "reflection-rewriter-runtime"
