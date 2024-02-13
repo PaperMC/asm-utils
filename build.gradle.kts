c3405053dd8e6ef0235b3da30225b926dbaa5cc2
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-02-13 10:33:27 -0700

    Setup publishing (#5)
    * Setup publishing
    
    * Populate pom
    
    * Setup javadoc linking
    
    * Reformat actions config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.incendo.cloudbuildlogic.jmp
+
 plugins {
     val indraVer = "3.1.3"
     id("net.kyori.indra") version indraVer
     id("net.kyori.indra.checkstyle") version indraVer
     id("net.kyori.indra.publishing") version indraVer
+    id("org.incendo.cloud-build-logic.javadoc-links") version "0.0.12"
 }
 
 allprojects {
@@ -13,6 +16,33 @@ allprojects {
     indra {
         javaVersions {
             target(17)
+        }
+
+        publishSnapshotsTo("paperSnapshots", "https://repo.papermc.io/repository/maven-snapshots/")
+        publishReleasesTo("paperReleases", "https://repo.papermc.io/repository/maven-releases/")
+
+        apache2License()
+
+        github("PaperMC", "asm-utils") {
+            ci(true)
+        }
+
+        configurePublications {
+            pom {
+                developers {
+                    jmp()
+                    developer {
+                        id = "Machine-Maker"
+                        name = "Jake Potrebic"
+                        url = "https://github.com/Machine-Maker"
+                    }
+                    developer {
+                        id = "kennytv"
+                        name = "Nassim Jahnke"
+                        url = "https://github.com/kennytv"
+                    }
+                }
+            }
         }
     }
 
